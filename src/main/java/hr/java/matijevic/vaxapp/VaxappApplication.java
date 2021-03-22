e931b6ba6aeba2d065c4b617e13020a51cb01efe
Nikola <matijevicnikola@yahoo.com>
2021-03-22 12:42:28 +0100

    Initial commit

---
+++ src/main/java/hr/java/matijevic/vaxapp/VaxappApplication.java
@@ -0,0 +1,13 @@
+package hr.java.matijevic.vaxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaxappApplication.class, args);
+    }
+
+}
