e8a4fef0cf052c08f15fed3c90ec95bdd0f25ae8
Nikola <matijevicnikola@yahoo.com>
2021-03-23 16:18:15 +0100

    Commit - labos1

---
+++ src/main/java/hr/java/matijevic/vaxapp/VaccineController.java
@@ -0,0 +1,30 @@
+package hr.java.matijevic.vaxapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("vaccine")
+public class VaccineController {
+
+
+    private final VaccineService vaccineService;
+
+    public VaccineController(VaccineService vaccineService) {
+        this.vaccineService = vaccineService;
+    }
+
+    @GetMapping
+    public List<VaccineDTO> getAllVaccine() {
+        return vaccineService.findAll();
+    }
+
+    @GetMapping(params = "researchName")
+    public VaccineDTO getVaccineByResearchName(@RequestParam final String researchName){
+        return vaccineService.findVaccineByResearchName(researchName);
+    }
+}
